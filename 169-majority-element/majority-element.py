50beb48db24157850bdddf70459ba812d4fe0c26
hintsete <167539044+hintsete@users.noreply.github.com>
2025-02-05 10:24:42 +0300

    Time: 7 ms (57.62%) | Memory: 19.3 MB (44.32%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -13,7 +13,10 @@ class Solution:
         for num in nums:
             if count==0:
                 res=num
-            count+=1 if res==num else -1
+            if res==num:
+                count+=1
+            else:
+                count-=1
            
 
 
