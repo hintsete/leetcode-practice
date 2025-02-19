9291cf9b0fc0d226e9d59cfd960a8371c75cef97
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-02-19 13:43:10 +0300

    Time: 69 ms (26.84%) | Memory: 42 MB (7.62%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -12,17 +12,26 @@ class Solution:
                 range_Sum[start]-=1
                 if end+1 < len(range_Sum):
                     range_Sum[end+1]+=1
-        #calculate the prefix sum and store it in range_Sum    
+         
         for i in range(1,len(range_Sum)):
             range_Sum[i]+=range_Sum[i-1]
-        # to find the number of shift to each char in s
+        
         shifted_chars=[]
         for i in range(len(s)):
             shift=range_Sum[i]%26
-            # after computing the shifted postion add ord(a) to map back to lower case letter
-            shifted_chars.append(chr((ord(s[i])-ord('a')+shift)%26 + ord('a')))
+            new_val=ord(s[i])+shift
+            if new_val>122:
+                new_val-=26
+            if new_val<97:
+                new_val+=26
+            shifted_chars.append(chr(new_val))
+        return "".join(shifted_chars)
+
+        #     # after computing the shifted postion add ord(a) to map back to lower case letter
+        #     shifted_chars.append(chr((ord(s[i])-ord('a')+shift)%26 + ord('a')))
+        # # print(ord("h")-ord("a"))
             
-        return "".join(shifted_chars[:len(s)])
+        # return "".join(shifted_chars[:])
 
         
     
