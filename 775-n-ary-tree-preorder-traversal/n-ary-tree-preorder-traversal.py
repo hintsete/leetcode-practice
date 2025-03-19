725762ceb230989ba205397ed57d76ac7b8e894c
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-19 15:37:33 +0300

    Time: 48 ms (62.74%) | Memory: 19.1 MB (91.78%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack=[root]
+        output=[]
+        while stack:
+            temp=stack.pop()
+            output.append(temp.val)
+            stack.extend(temp.children[::-1])
+        return output
+       
+        
