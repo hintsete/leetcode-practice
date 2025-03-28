86b65f82ae4a470e3e4084d69df78b9d3dd90a95
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-03-28 10:29:28 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (21.14%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -15,31 +15,64 @@ class Solution:
         #     elif nums[mid]<target:
         #         left=mid+1
         # return ans
+        # start=end=-1
+        # def first_pos(l,r):
+        #     # nonlocal start
+        #     while l<=r:
+        #         mid=(l+r)//2
+        #         if nums[mid]<target:
+        #             l=mid+1
+        #         else:
+        #             # if nums[mid]==target:
+        #             #     start=mid
+        #             r=mid-1
+        #     return l if l<len(nums) and nums[l]==target else -1
+        # def last_pos(l,r):
+        #     # nonlocal end
+        #     while l<=r:
+        #         mid=(l+r)//2
+        #         if nums[mid]>target:
+        #             r=mid-1
+        #         else:
+        #             # if nums[mid]==target:
+        #             #     end=mid
+        #             l=mid+1
+        #     return r if r>=0 and nums[r]==target else -1
+        # first,last=first_pos(0,len(nums)-1),last_pos(0,len(nums)-1)
+        # return [first,last]
         start=end=-1
-        def first_pos(l,r):
+        n=len(nums)
+        def first_pos(l,r,nums,target):
             nonlocal start
             while l<=r:
                 mid=(l+r)//2
-                if nums[mid]<target:
+                if nums[mid]==target:
+                    start=mid
+                    r=mid-1
+                elif nums[mid]<target:
                     l=mid+1
+
                 else:
-                    if nums[mid]==target:
-                        start=mid
                     r=mid-1
             return start
-        def last_pos(l,r):
+        def last_pos(l,r,nums,target):
             nonlocal end
             while l<=r:
                 mid=(l+r)//2
-                if nums[mid]>target:
-                    r=mid-1
-                else:
-                    if nums[mid]==target:
-                        end=mid
+                if nums[mid]==target:
+                    end=mid
                     l=mid+1
+                elif nums[mid]<target:
+                    l=mid+1
+
+                else:
+                    r=mid-1
             return end
-        first,last=first_pos(0,len(nums)-1),last_pos(0,len(nums)-1)
+        first,last=first_pos(0,n-1,nums,target),last_pos(0,n-1,nums,target)
         return [first,last]
+        
+
+
             
 
 
