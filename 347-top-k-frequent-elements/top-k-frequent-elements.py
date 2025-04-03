7a9a86d936a4e27d34badcfa41608dee459a90e9
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-04-03 17:59:47 +0300

    Time: 0 ms (100.00%) | Memory: 21.1 MB (89.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count={}
-        frequency=[[] for _ in range(len(nums)+1)]
-        for n in nums:
-            count[n]=1+count.get(n,0)
+        # count=defaultdict(int)
+        count=Counter(nums)
+        max_val=max(count.values())
+        frequency=[[] for _ in range(max_val+1)]
+        
         # c is the number of counts used as an index in frequency 2d list and n is the list of values with that number of occurence
         for n,c in count.items():
             frequency[c].append(n)
 
+        print(frequency)
+
         # now iterate through frequency in reverse order to pop the frequently occured number
         res=[]
-        # here i starts from the largest index which is the largest count
-        for i in range(len(frequency)-1,0,-1):
+        
+        for i in range(max_val,0,-1):
             for n in frequency[i]:
                 res.append(n)
                 while len(res)==k:
                     return res
+                
+            
+   
+
+        
         
