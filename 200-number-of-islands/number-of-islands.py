b2592503cb819ce88c8da0623c58259919c1aba8
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-04-12 00:07:23 +0300

    Time: 220 ms (98.31%) | Memory: 20.3 MB (58.62%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        direction=[[-1,0],[1,0],[0,-1],[0,1]]
-        def inbound(row,col):
-            return 0<=row<len(grid) and 0<=col<len(grid[0])
-        visited=set()
-
+        n , m = len(grid) , len(grid[0])
 
         def dfs(r,c):
-            if not inbound(r,c) or (r,c)  in visited or grid[r][c]=="0" :
-                return 0
+            if r >= n or c >= m or r < 0 or c < 0 or grid[r][c] == "0":
+                return
+            else:
+                grid[r][c] = "0"
+                dfs(r , c-1)
+                dfs(r , c+1)
+                dfs(r-1 , c)
+                dfs(r+1 , c)
 
-            ans=int(grid[r][c])
-            visited.add((r,c))
-            for dx,dy in direction:
-                new_r=dx+r
-                new_c=dy+c
-                dfs(new_r,new_c)
-        res=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1" and (i,j) not in visited:
-                    res+=1
+
+        island=0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    island += 1
                     dfs(i,j)
-        return res
-
             
+        return island
+     
 
         
