bdb65915a27828c86dcb720b349193c52d739edd
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-05-15 17:49:05 +0300

    Time: 59 ms (15.26%) | Memory: 18.6 MB (22.81%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -3,14 +3,12 @@ class Solution:
         ans=[]
         for i in range(n+1):
             count=0
-            x=i
-            while x>0:
-                if x%2==1:
+            while i:
+                if i&1:
                     count+=1
-
-                x=x//2
-
+                i=i>>1
             ans.append(count)
+            
                 
         return ans
     
