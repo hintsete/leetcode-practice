93d4e7135766ded827cbf8208a70f538527a7646
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-10-03 16:57:18 +0300

    Time: 467 ms (56.83%) | Memory: 42.9 MB (57.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,23 +1,36 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo={}
-        def dp(i,j):
-            #base case
-            if i>=len(text1) or j>= len(text2):
-                return 0
-            # print(i,j,memo)
-            if (i,j) in memo:
-                return memo[i,j]
-            if text1[i]==text2[j]:
-                memo[i,j]=1+dp(i+1,j+1)
-            else:
-                memo[i,j]=max(dp(i+1,j),dp(i,j+1))
-            # memo[i,j]=max(1+dp(i+1,j+1),dp(i+1,j+1))
+        # memo={}
+        # def dp(i,j):
+        #     #base case
+        #     if i>=len(text1) or j>= len(text2):
+        #         return 0
+        #     # print(i,j,memo)
+        #     if (i,j) in memo:
+        #         return memo[i,j]
+        #     if text1[i]==text2[j]:
+        #         memo[i,j]=1+dp(i+1,j+1)
+        #     else:
+        #         memo[i,j]=max(dp(i+1,j),dp(i,j+1))
+        #     # memo[i,j]=max(1+dp(i+1,j+1),dp(i+1,j+1))
 
 
-            # count=0
-            return memo[i,j]
+        #     # count=0
+        #     return memo[i,j]
         
 
-        return dp(0,0)
+        # return dp(0,0)
+        n=len(text1)
+        m=len(text2)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j]= 1+ dp[i-1][j-1]
+                else:
+                    p1=dp[i-1][j] 
+                    p2=dp[i][j-1] 
+                    dp[i][j]=max(p1,p2)
+
+        return dp[n][m]
         
