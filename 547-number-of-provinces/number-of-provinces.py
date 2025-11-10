5a55a31416865900eb64880e8ef93389f91ed224
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2025-11-10 23:20:53 +0300

    Time: 7 ms (55.98%) | Memory: 19.2 MB (46.00%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,48 +1,30 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n=len(isConnected)
-        parent={i:i for i in range(n)}
-        rank={i:1 for i in range(n)}
+        province=set()
+        root={i:i for i in range(n)}
         def find(x):
-            if parent[x]!=x:
-                parent[x]=find(parent[x])
-                
+            if x==root[x]:
+                return x
 
-            return parent[x]
+            root[x]=find(root[x])
+            return root[x]
+
         def union(x,y):
             p1=find(x)
             p2=find(y)
             if p1!=p2:
-                if rank[p1]>rank[p2]:
-                    parent[p2]=p1
-                elif rank[p1]<rank[p2]:
-                    parent[p1]=p2
-                else:
-                    parent[p2]=p1
-                    rank[p1]+=1
-        connected=n
+                root[p1]=p2
+
+           
         for i in range(n):
             for j in range(i+1,n):
-                if isConnected[i][j] and find(i)!=find(j):
-                    connected-=1
+                if isConnected[i][j]:
                     union(i,j)
-                    
-        return connected
-       
+        
+        for i in range(n):
+            province.add(find(i))
 
-        # visited=[False]*n
-        # def dfs(node):
-        #     for neigh in range(n):
-        #         if isConnected[node][neigh]==1 and not visited[neigh]:
-        #             visited[node]=True
-        #             dfs(neigh)
-      
-
-        # province=0
-        # for node in range(n):
-        #     if not visited[node]:
-        #         visited[node]=True
-        #         dfs(node)
-        #         province+=1
             
-        # return province
+
+        return len(province)
