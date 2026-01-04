c53651f13bf4000f96280cf9e2d16bcfbf8a7cda
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2026-01-04 11:58:23 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (84.99%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -5,11 +5,20 @@ class Solution:
         #no of papers with highest citation
         n=len(citations)
         h=0
-        citations.sort(reverse=True)
-        for i in range(n):
-            if i+1<=citations[i]:
-                h+=1
+        citations.sort()
+        l=0  
+        r=n-1
+        while l<=r:
+            mid=(l+r)//2
+            if citations[mid]>=n-mid:
+                print(mid)
+                h=n-mid
+                
+                r=mid-1
+            else:
+                l=mid+1
         return h
+
       
 
         
