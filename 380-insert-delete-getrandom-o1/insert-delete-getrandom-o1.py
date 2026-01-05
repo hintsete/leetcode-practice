032b8e1a4f201801fa6e3a1d989e22e923621966
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2026-01-05 23:42:51 +0300

    Time: 357 ms (5.58%) | Memory: 54.9 MB (75.77%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,47 +1,27 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.map={}
-        self.my_list=list()
+        self.set=set()
         
 
     def insert(self, val: int) -> bool:
-        # if not(val  in self.my_set):
-        #     self.my_set[val]=len(self.my_list)
-        #     self.my_list.append(val)
-        #     return True
-        # return False
-        res= val not in self.map
-        if  res:
-            self.map[val]=len(self.my_list)
-            self.my_list.append(val)
-        return res
-        
-      
+        if val not in self.set:
+            self.set.add(val)
+            return True
+        return False
         
 
     def remove(self, val: int) -> bool:
-        # if val in self.my_set:
-        #     remove_index=self.my_set[val]
-        #     del 
-        #     self.my_list[self.my_list[-1]]=
-
-        #     return True
-        # else:
-        #     return False
-        res= val in self.map
-        if res:
-            idx=self.map[val]
-            lastval=self.my_list[-1]
-            self.my_list[idx]=lastval
-            self.my_list.pop()
-            self.map[lastval]=idx
-            del self.map[val]
-        return res
+        if val in self.set:
+            self.set.remove(val)
+            return True
+        return False
         
 
     def getRandom(self) -> int:
-        return random.choice(self.my_list)
+        x=random.choice(list(self.set))
+        return x
+        
         
 
 
