70e93957d697b37d47d9ab67e33da5122e41a093
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2026-01-07 00:12:15 +0300

    Time: 43 ms (12.17%) | Memory: 27 MB (8.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,19 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # set up left and right multiplier to 1 to take care of edge cases
-        left_mul=1
-        right_mul=1
-        size=len(nums)
-        # create a left and right array to actually find the product of an element except itself
-        left_arr=[0]*size
-        right_arr=[0]*size
+        ans=[]
+        prefix=[1]
+        suffix=[1]
+        for i in range(len(nums)-1,-1,-1):
+            res=suffix[-1]*nums[i]
+            suffix.append(res)
+        for num in nums:
+            res=prefix[-1]*num
+            prefix.append(res)
+        suffix=suffix[::-1]
+        print(prefix)
+        print(suffix)
+        for i in range(1,len(prefix)):
+            res=prefix[i-1]*suffix[i]
+            ans.append(res)
+        print(ans)
+        return ans
 
-        for i in range(size):
-            j=-1-i
-            left_arr[i]=left_mul
-            right_arr[j]=right_mul
-            left_mul*=nums[i]
-            right_mul*=nums[j]
-        return [l*r for l,r in zip(left_arr,right_arr)]
+       
         
-            
