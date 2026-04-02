113ecd096af4682abeddebd8263146117cddf835
Hintsete Hilawe <167539044+hintsete@users.noreply.github.com>
2026-04-02 23:12:58 +0300

    Time: 39 ms (5.54%) | Memory: 20.9 MB (6.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,8 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count_1=Counter(s)
-        count_2=Counter(t)
-        if count_1==count_2:
-            return True
-        else:
+        s=list(s)
+        print(s)
+        s_map=Counter(s)
+        print(s_map)
+
+        t=list(t)
+        print(t)
+        t_map=Counter(t)
+        print(t_map)
+
+        if len(t)!=len(s):
             return False
+
+        for ch in t:
+            if ch in s_map:
+                t_map[ch]-=1
+                s_map[ch]-=1
+            if t_map[ch]==0:
+                del t_map[ch]
+            if s_map[ch]==0:
+                del s_map[ch]
+        return len(t_map)==0 and len(s_map)==0
+        
